e8fd1bd1981f64e34861a8edf3076a5e48f3d89f
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-19 16:14:29 +0200

    Add payment group and payment date to counterpart lines tree (grafted from d6010ac15233ffaae0c4bd6bdaf59206d0cf66b3) (grafted from 0e23a11056760e10096156747e2b7f858ceabb07)

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from statement import *
+from .account import *
+from .statement import *
 
 
 def register():
     Pool.register(
+        MoveLine,
         StatementLine,
         Group,
         module='account_bank_statement_payment', type_='model')
